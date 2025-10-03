0f07e5b8e8d36e2ecb7bf96f50d2d5f7cf6bf3be
Sontsevolosa <sontsevolosa@gmail.com>
2025-10-03 16:49:06 +0300

    Improve code

--- app/main.py
+++ app/main.py
@@ -1,17 +1,22 @@
 def copy_file(command: str) -> None:
-    splitted_entry_command = command.strip().split()
+    splitted_entry_command = command.split()
 
-    if len(splitted_entry_command) != 3 or splitted_entry_command[0] != "cp":
+    if len(splitted_entry_command) != 3:
+        return
+
+    if splitted_entry_command[0] != "cp":
+        return
+
+    if splitted_entry_command[1] == splitted_entry_command[2]:
         return
 
     file_to_copy = splitted_entry_command[1]
     new_file = splitted_entry_command[2]
 
-    if file_to_copy == new_file:
-        return
-
     try:
-        with open(file_to_copy, "r") as f_in, open(new_file, "w") as f_out:
-            f_out.write(f_in.read())
+        with (open(file_to_copy, "r") as input_file,
+              open(new_file, "w") as output_file):
+            content = input_file.read()
+            output_file.write(content)
     except FileNotFoundError:
         return
